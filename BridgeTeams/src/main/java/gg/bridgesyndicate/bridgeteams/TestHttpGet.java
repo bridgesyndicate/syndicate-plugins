69eeae531539514baa9a5a8a26d7ffb057187e33
Kenneth Berland <ken@hero.net>
2021-09-24 09:12:28 -0700

    update service arns for pipeline

--- BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/TestHttpGet.java
+++ BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/TestHttpGet.java
@@ -23,10 +23,11 @@ public class TestHttpGet {
     private static void doTestPut() throws URISyntaxException, IOException {
         boolean development = false;
         String uuid = "c996dae3-433c-48f4-8a90-d4ea2d50f2a6";
-        String taskArn = "arn:aws:ecs:us-west-2:595508394202:task/default/5b42900b6d094b249db5315b7cb83612";
+        String taskArn = "10.18.0.1";
         ObjectMapper mapper = new ObjectMapper();
         ArnUpdatePayload arnUpdatePayload = new ArnUpdatePayload(uuid, taskArn);
         String payload = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(arnUpdatePayload);
+        System.out.println(payload);
         SyndicateWebServiceHttpClient syndicateWebServiceHttpClient =
                 new SyndicateWebServiceHttpClient("/auth/game/container_metadata", HttpMethodName.PUT, development);
         int httpStatus = syndicateWebServiceHttpClient.put(payload);
--- scripts/ecs-save-money.sh
+++ scripts/ecs-save-money.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-set -e
+set -evx
 if [[ -z $1 ]]
 then
     echo 'set the number of desired instances'
     echo 'e.g:'
     echo $0 1
 else
-    readarray -t arns < <(aws ecs list-services --cluster syndicate-bungee-01 | jq -r '.serviceArns[]')
+    CLUSTER=SyndicateECSCluster
+    readarray -t arns < <(aws ecs list-services --cluster $CLUSTER | \
+			      jq -r '.serviceArns[]')
     for arn in "${arns[@]}"
     do
 	echo setting $arn
 	aws ecs update-service \
 	    --service $arn \
-	    --cluster syndicate-bungee-01 \
+	    --cluster $CLUSTER \
 	    --desired-count $1 | \
 	    jq '.service.desiredCount'
     done
