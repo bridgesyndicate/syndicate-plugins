a19f3645e5b425e704a51c4d1f7c901ac4843f00
lmao <lmao>
2021-11-09 20:42:12 +0000

    Squashed commit of the following
    commit 6dde83a6 
    Author: bdamja <iceicebabyalter@gmail.com> 
    Date: Mon Nov 08 2021 21:26:32 GMT-0700 (Mountain Standard Time) 
    
        added proper chat prefixes for players and spectators

--- BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ChatHandler.java
+++ BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ChatHandler.java
@@ -2,6 +2,7 @@ package gg.bridgesyndicate.bridgeteams;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,14 +10,29 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatHandler implements Listener {
 
+    private static final String PLAYER_PREFIX = ChatColor.GREEN + "[GAME] ";
+    private static final String SPECTATOR_PREFIX = ChatColor.GRAY + "[SPECTATOR] ";
+
     public ChatHandler() {
     }
 
     @EventHandler
     public static void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
-        String msg = event.getMessage();
+        String message = event.getMessage();
+        if(player.getGameMode().equals(GameMode.SPECTATOR)){
+            broadcastSpectatorMessage(player, message);
+        } else {
+            broadcastPlayerMessage(player, message);
+        }
         event.setCancelled(true);
-        Bukkit.broadcastMessage(ChatColor.GREEN + "[GAME] " + player.getName() + ChatColor.WHITE + ": " + msg);
+    }
+
+    private static void broadcastSpectatorMessage(Player spectator, String message){
+        Bukkit.broadcastMessage(SPECTATOR_PREFIX + spectator.getName() + ": " + message);
+    }
+
+    private static void broadcastPlayerMessage(Player player, String message){
+        Bukkit.broadcastMessage(PLAYER_PREFIX + MatchTeam.getChatColor(player) + player.getName() + ChatColor.WHITE + ": " + message);
     }
 }
