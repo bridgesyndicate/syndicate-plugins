949436792ab295f5d76a5352099a004acaa12fba
Kenneth Berland <ken@hero.net>
2021-11-09 20:40:55 -0800

    add -Xmx1g and --noconsole to java args

--- run-spigot.sh
+++ run-spigot.sh
@@ -5,7 +5,7 @@ do
     $(./pick-map.rb)
     echo picked map $SYNDICATE_MAP_NAME
     rm -rf world && tar -xf world.tar.gz
-    java -DmapName=$SYNDICATE_MAP_NAME -jar spigot-1.8.8.jar nogui
+    java -Xmx1g -DmapName=$SYNDICATE_MAP_NAME -jar spigot-1.8.8.jar nogui --noconsole
     rm -rf world # clean up
     sleep 1
 done
