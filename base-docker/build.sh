d275b3c22390076f0dda034e74e878e29ae38350
Kenneth Berland <ken@hero.net>
2022-01-21 21:25:25 -0800

    Fixes for log4j CVE-2021-44228 and CVE-2021-45046. Rebuit spigot 1.8.8 with patches from https://us-west-2.console.aws.amazon.com/codesuite/codecommit/repositories/syndicate-spigot/browse?region=us-west-2 Do sed instead of patch.

--- base-docker/build.sh
+++ base-docker/build.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 set -ve
-VERSION=12
-echo version $VERSION
-echo building with the contents of minecraft-home
-echo edit if you don\'t have this directory
-tar -cJf minecraft-home.${VERSION}.tar.xz minecraft-home/
-aws s3 cp minecraft-home.${VERSION}.tar.xz s3://syndicate-minecraft-artifacts/
-mv minecraft-home.${VERSION}.tar.xz minecraft-home.tar.xz
-# aws s3 cp s3://syndicate-minecraft-artifacts/minecraft-home.${VERSION}.tar.xz minecraft-home.tar.xz
+
+VERSION=13
 REPOSITORY_HOST=595508394202.dkr.ecr.us-west-2.amazonaws.com
 REPOSITORY_URI=$REPOSITORY_HOST/syn-minecraft-dist
+
+if [ ! -d "minecraft-home" ]; then
+    echo downloading version $VERSION of minecraft home from s3
+    aws s3 cp s3://syndicate-minecraft-artifacts/minecraft-home.${VERSION}.tar.xz minecraft-home.tar.xz
+else
+    echo building version $VERSION with the contents of minecraft-home
+    tar -cJf minecraft-home.${VERSION}.tar.xz minecraft-home/
+    echo sending it to the cloud
+    aws s3 cp minecraft-home.${VERSION}.tar.xz s3://syndicate-minecraft-artifacts/
+fi
+
+mv minecraft-home.${VERSION}.tar.xz minecraft-home.tar.xz
 aws ecr get-login-password  | docker login --username AWS --password-stdin $REPOSITORY_HOST
 docker build -t $REPOSITORY_URI:latest .
 docker push $REPOSITORY_URI:latest
