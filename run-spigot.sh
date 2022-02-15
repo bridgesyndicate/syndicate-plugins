cf4e99d57b01c0221c6aeacc7b92b1706db89ad4
Kenneth Berland <ken@hero.net>
2022-02-15 13:59:30 -0800

    Revert "TEST. Don't delete the world after a game."
    This reverts commit 43a613266954936468b479df938064d3b8405acd.
    
    Conflicts:
    	run-spigot.sh

--- run-spigot.sh
+++ run-spigot.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -evx
-
-$(./pick-map.rb)
-echo picked map $SYNDICATE_MAP_NAME
-rm -rf world && tar -xf world.tar.gz
-
 while :
 do
+    $(./pick-map.rb)
+    echo picked map $SYNDICATE_MAP_NAME
+    rm -rf world && tar -xf world.tar.gz
     java -Xms1536M -Xmx1536M -XX:+UseG1GC -DmapName=$SYNDICATE_MAP_NAME -jar spigot-1.8.8.jar nogui --noconsole
+    rm -rf world # clean up
     sleep 1
 done
