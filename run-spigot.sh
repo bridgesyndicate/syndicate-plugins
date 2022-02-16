9bce0b118198b974b092822f3a312fccc5dbb046
Kenneth Berland <ken@hero.net>
2022-02-15 16:33:38 -0800

    leave more RAM for other things

--- run-spigot.sh
+++ run-spigot.sh
@@ -5,7 +5,7 @@ do
     $(./pick-map.rb)
     echo picked map $SYNDICATE_MAP_NAME
     rm -rf world && tar -xf world.tar.gz
-    java -Xms1000M -Xmx1000M -XX:+UseG1GC -DmapName=$SYNDICATE_MAP_NAME -jar spigot-1.8.8.jar nogui --noconsole
+    java -Xms768M -Xmx768M -XX:+UseG1GC -DmapName=$SYNDICATE_MAP_NAME -jar spigot-1.8.8.jar nogui --noconsole
     rm -rf world # clean up
     sleep 1
 done
