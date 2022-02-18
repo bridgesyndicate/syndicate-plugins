bdbd85c7d291c9ea7e55041425238a1e535c8593
bdamja <iceicebabyalter@gmail.com>
2022-02-18 13:30:24 -0700

    Revert "Merge branch 'master' of ssh://git-codecommit.us-west-2.amazonaws.com/v1/repos/syndicate-plugins"
    This reverts commit 73dd729d83df8e6b64f61cea97ea1a7c6bb93311, reversing
    changes made to b9db9ecc4ee14074d8c41196adf57790c6a9e68d.

--- BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ClickHandler.java
+++ BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ClickHandler.java
@@ -1,44 +1,44 @@
-//package gg.bridgesyndicate.bridgeteams;
-//
-//
-//import net.minecraft.server.v1_8_R3.MinecraftServer;
-//import org.bukkit.ChatColor;
-//import org.bukkit.entity.Player;
-//import org.bukkit.event.EventHandler;
-//import org.bukkit.event.Listener;
-//import org.bukkit.event.block.BlockPlaceEvent;
-//
-//import java.util.HashMap;
-//import java.util.UUID;
-//
-//public class ClickHandler implements Listener {
-//
-//    public ClickHandler(){}
-//
-//    public static HashMap<UUID, Integer> lastBlockPlaceTimestamp = new HashMap<>();
-//    public static HashMap<UUID, Integer> lastPlaced2BlocksIn2Ticks = new HashMap<>();
-//
-//    @EventHandler
-//    public void onBlockPlace(BlockPlaceEvent event) {
-//
-//        Player player = event.getPlayer();
-//        UUID id = player.getUniqueId();
-//        if(lastPlaced2BlocksIn2Ticks.get(id) != null) {
-//            if(MinecraftServer.currentTick - lastPlaced2BlocksIn2Ticks.get(id) < 2){
-//                player.sendMessage(ChatColor.DARK_RED + "Prevented right-click mouse abuse.");
-//                event.setCancelled(true);
-//                // 3) if player places 3 blocks in 3 ticks, cancel
-//            }
-//            lastPlaced2BlocksIn2Ticks.remove(id);
-//        }
-//        if(lastBlockPlaceTimestamp.get(id) != null) {
-//            if ((MinecraftServer.currentTick - lastBlockPlaceTimestamp.get(id)) < 2) {
-//                lastPlaced2BlocksIn2Ticks.put(id, MinecraftServer.currentTick);
-//                // 2) mark down when a player places 2 blocks in 2 ticks
-//            }
-//        }
-//        lastBlockPlaceTimestamp.put(id, MinecraftServer.currentTick);
-//        // 1) plot timestamp of when player places a block
-//    }
-//
-//}
+package gg.bridgesyndicate.bridgeteams;
+
+
+import net.minecraft.server.v1_8_R3.MinecraftServer;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ClickHandler implements Listener {
+
+    public ClickHandler(){}
+
+    public static HashMap<UUID, Integer> lastBlockPlaceTimestamp = new HashMap<>();
+    public static HashMap<UUID, Integer> lastPlaced2BlocksIn2Ticks = new HashMap<>();
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+
+        Player player = event.getPlayer();
+        UUID id = player.getUniqueId();
+        if(lastPlaced2BlocksIn2Ticks.get(id) != null) {
+            if(MinecraftServer.currentTick - lastPlaced2BlocksIn2Ticks.get(id) < 2){
+                player.sendMessage(ChatColor.DARK_RED + "Prevented right-click mouse abuse.");
+                event.setCancelled(true);
+                // 3) if player places 3 blocks in 3 ticks, cancel
+            }
+            lastPlaced2BlocksIn2Ticks.remove(id);
+        }
+        if(lastBlockPlaceTimestamp.get(id) != null) {
+            if ((MinecraftServer.currentTick - lastBlockPlaceTimestamp.get(id)) < 2) {
+                lastPlaced2BlocksIn2Ticks.put(id, MinecraftServer.currentTick);
+                // 2) mark down when a player places 2 blocks in 2 ticks
+            }
+        }
+        lastBlockPlaceTimestamp.put(id, MinecraftServer.currentTick);
+        // 1) plot timestamp of when player places a block
+    }
+
+}
