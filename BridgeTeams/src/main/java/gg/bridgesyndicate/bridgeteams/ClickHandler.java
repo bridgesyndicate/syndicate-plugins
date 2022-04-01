bed238bdecfc812ac2e110108a97e2abd3c76a87
bdamja <iceicebabyalter@gmail.com>
2022-03-31 23:04:44 -0600

    removed double clicking prevention

--- BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ClickHandler.java
+++ BridgeTeams/src/main/java/gg/bridgesyndicate/bridgeteams/ClickHandler.java
@@ -21,32 +21,33 @@ public class ClickHandler implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-
-        Player player = event.getPlayer();
-        UUID id = player.getUniqueId();
-        if(lastPlaced3BlocksIn3Ticks.get(id) != null) {
-            if(MinecraftServer.currentTick - lastPlaced3BlocksIn3Ticks.get(id) < 2){
-                player.sendMessage(ChatColor.DARK_RED + "Prevented right-click mouse abuse.");
-                event.setCancelled(true);
-                // 4) if player places 4 blocks in 4 ticks, cancel
-            }
-            lastPlaced3BlocksIn3Ticks.remove(id);
-        }
-        if(lastPlaced2BlocksIn2Ticks.get(id) != null) {
-            if(MinecraftServer.currentTick - lastPlaced2BlocksIn2Ticks.get(id) < 2){
-                lastPlaced3BlocksIn3Ticks.put(id, MinecraftServer.currentTick);
-                // 3) mark down when a player places 3 blocks in 3 ticks
-            }
-            lastPlaced2BlocksIn2Ticks.remove(id);
-        }
-        if(lastBlockPlaceTimestamp.get(id) != null) {
-            if ((MinecraftServer.currentTick - lastBlockPlaceTimestamp.get(id)) < 2) {
-                lastPlaced2BlocksIn2Ticks.put(id, MinecraftServer.currentTick);
-                // 2) mark down when a player places 2 blocks in 2 ticks
-            }
-        }
-        lastBlockPlaceTimestamp.put(id, MinecraftServer.currentTick);
-        // 1) plot timestamp of when player places a block
+//
+//        Player player = event.getPlayer();
+//        UUID id = player.getUniqueId();
+//        if(lastPlaced3BlocksIn3Ticks.get(id) != null) {
+//            if(MinecraftServer.currentTick - lastPlaced3BlocksIn3Ticks.get(id) < 2){
+//                player.sendMessage(ChatColor.DARK_RED + "Prevented right-click mouse abuse.");
+//                event.setCancelled(true);
+//                // 4) if player places 4 blocks in 4 ticks, cancel
+//            }
+//            lastPlaced3BlocksIn3Ticks.remove(id);
+//        }
+//        if(lastPlaced2BlocksIn2Ticks.get(id) != null) {
+//            if(MinecraftServer.currentTick - lastPlaced2BlocksIn2Ticks.get(id) < 2){
+//                lastPlaced3BlocksIn3Ticks.put(id, MinecraftServer.currentTick);
+//                // 3) mark down when a player places 3 blocks in 3 ticks
+//            }
+//            lastPlaced2BlocksIn2Ticks.remove(id);
+//        }
+//        if(lastBlockPlaceTimestamp.get(id) != null) {
+//            if ((MinecraftServer.currentTick - lastBlockPlaceTimestamp.get(id)) < 2) {
+//                lastPlaced2BlocksIn2Ticks.put(id, MinecraftServer.currentTick);
+//                // 2) mark down when a player places 2 blocks in 2 ticks
+//            }
+//        }
+//        lastBlockPlaceTimestamp.put(id, MinecraftServer.currentTick);
+//        // 1) plot timestamp of when player places a block
+        // temporarily disabled
     }
 
 }
